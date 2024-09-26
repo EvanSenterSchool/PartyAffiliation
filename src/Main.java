ad7ce2989cb0777778ec43d695a2fac61807cff9
614925 <senter.614925@swl.k12.oh.us>
2024-09-26 08:31:26 -0400

    hi

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String partyType;
+        String partyDemocrat = "D";
+        String partyRepublican = "R";
+        String partyIndependent = "I";
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("What political party are you involved in? Options are D, R, and I ");
+
+       if (scan.hasNextLine())   {
+
+        partyType = scan.nextLine();
+
+            if (partyType.equals(partyDemocrat))
+                System.out.println("You get a Democratic Donkey.");
+            else if (partyType.equals(partyRepublican))
+                System.out.println("You get a Republican Elephant.");
+            else if (partyType.equals(partyIndependent))
+                System.out.println("You get an Independent Person.");
+
+
+		else
+                System.out.println("You have entered the wrong data type");
+        }
     }
 }
